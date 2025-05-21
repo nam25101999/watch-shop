feb383b72d41cfd0d0602bbdce9e0ccc34ea292d
Your Name <your.email@example.com>
2025-05-22 03:21:03 +0700

    Fix DisallowedHost issue by updating ALLOWED_HOSTS

--- watchshop/settings.py
+++ watchshop/settings.py
@@ -1,9 +1,8 @@
-import os
+ALLOWED_HOSTS = ['watch-shop-5ch9.onrender.com', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 from pathlib import Path
-from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
